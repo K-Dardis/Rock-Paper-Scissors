bd6c1739c5055f9c673282fb3ad384618ce15370
K-Dardis <K.J.J.A.Dardis@outlook.com>
2023-04-20 13:16:38 +0100

    Added a comparison of hands to determine winner

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 #create a list of playable hands
 hands = ["Rock", "Paper", "Scissors"]
 
@@ -13,3 +12,24 @@ computer = hands[randint(0,2)]
 #output Player and Computer hands
 print("Player hand was:", player)
 print("Computer hand was:", computer)
+
+#compare hands to determine who wins
+if player == computer:
+    print("It is a Tie!")
+elif player == "Rock":
+    if computer == "Paper":
+        print("You lose!")
+    else:
+        print("You win!")
+elif player == "Paper":
+    if computer == "Scissors":
+        print("You lose")
+    else:
+        print("You win!")
+elif player == "Scissors":
+    if computer == "Rock":
+        print("You lose")
+    else:
+        print("You win!")
+else:
+    print("I dont beleive that is a valid hand!")
